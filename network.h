24439d255c4b3ea844c3a4b265326d329d420bc7
7140709 <7140709@HLC4K07-09>
2019-05-01 19:52:24 -0500

    defined header files for network and neural classes, testing upload to github

--- network.h
+++ network.h
@@ -1,1 +1,30 @@
-#pragma once
+#ifndef NETWORK_H
+#define NETWORK_H
+
+#include <iostream>
+#include <string>
+
+
+class Network
+{
+public:
+	Network();
+	~Network();
+	void train();
+	void test()
+	void weights();
+	void loadweights();
+	//training data set
+	float *input;
+	float *output;
+
+private:
+	void inputLayer();
+	void outputLayer();
+	void middleLayer();
+	Neural * neuronptr;
+
+
+};
+
+#endif  //NEURAL_H
--- neural.h
+++ neural.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef NEURAL_H
+#define NEURAL_H
+
+#include <iostream>
+#include <string>
+
+
+class Neural
+{
+public:
+	Neural();
+	~Neural();
+
+	int y;
+protected:
+	float *x;
+	float *error;
+	void Normalize(Neural * neuronptr);
+	float findMin();
+	float findMax();
+
+};
+
+#endif  //NEURAL_H
