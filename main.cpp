5d83033ad0566496ca1fe817096de715f44e6ad8
7140709 <7140709@HLC4K07-09>
2019-05-06 19:53:57 -0500

    updated main.cpp to run with readDataFile

--- main.cpp
+++ main.cpp
@@ -3,10 +3,24 @@
 
 int main()
 {
-	Network n;
+	Network * neta = new Network;
 	std::string file = "configuration.txt";
 	std::ifstream cfgfile;
-	readConfigFile(cfgfile, file);
+	readConfigFile(cfgfile, file, neta);
 
+	//Network n;
+	std::string datafile = "datatest2.txt";
+	std::ifstream dfile;
+
+	//you can create class after you got io pairs
+
+	readDataFile(dfile, datafile, neta);
+	//Network n; //= new Network;
+	neta->displayVariables();
+	neta->displayInput();
+	//delete n;
+	//n = nullptr;
+	delete neta;
+	neta = nullptr;
 	return 0;
 }
